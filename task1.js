d4f35050e55b1a5234532de8ec28c53e86355063
pawan08bhd <pawanraghuwanshi16@gmail.com>
2025-07-18 22:14:16 +0000

    task1 complete

---
+++ task1.js
@@ -0,0 +1,4 @@
+// JavaScript for interactivity
+document.getElementById('interactiveBtn').addEventListener('click', function() {
+    alert('Congratulations! You just used JavaScript to create this alert message.');
+});
